6ea924a312102a7c38b9dec1192ce33a99dc5d74
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-09-04 12:17:28 +0200

    Generate a new component

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 `ng new first-app-angular --style=scss --skip-tests=true`
 
+# Generate a new component
+
+`ng generate component face-snap`
 
---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent {}
+
+// @ => décorateur. C'est un mécanisme qui permet d'ajouter des métadonnées à une classe, un attribut, une méthode, etc.
