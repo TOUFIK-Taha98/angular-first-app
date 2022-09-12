15ca73faa0b058ad84d0f53ccd18626dac003bc1
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-09-12 13:32:58 +0200

    showing all the FaceSnaps using the directive ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,35 +7,64 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  faceSnaps!: FaceSnap[];
   mySnap!: FaceSnap;
   autherSnap!: FaceSnap;
   thirdSnap!: FaceSnap;
   ngOnInit() {
-    this.mySnap = {
-      title: 'Archibald',
-      description: 'Mon meilleur amis depuis tout petit',
-      createdDate: new Date(),
-      snaps: 0,
-      imageUrl: "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg",
-      snapped: false,
-      location: 'Paris'
-    };
-    this.autherSnap = {
-      title: 'Testing',
-      description: '2eme component',
-      createdDate: new Date(),
-      snaps: 6,
-      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg",
-      snapped: false,
-      location: 'La montagne'
-    };
-    this.thirdSnap = {
-      title: 'Bobby',
-      description: 'Enfin un ami',
-      createdDate: new Date(),
-      snaps: 23,
-      imageUrl: "https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg",
-      snapped: false
-    };
+    this.faceSnaps = [
+      {
+        title: 'Archibald',
+        description: 'Mon meilleur amis depuis tout petit',
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg",
+        snapped: false,
+        location: 'Paris'
+      },
+      {
+        title: 'Testing',
+        description: '2eme component',
+        createdDate: new Date(),
+        snaps: 6,
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg",
+        snapped: false,
+        location: 'La montagne'
+      },
+      {
+        title: 'Bobby',
+        description: 'Enfin un ami',
+        createdDate: new Date(),
+        snaps: 23,
+        imageUrl: "https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg",
+        snapped: false
+      },
+      {
+        title: 'Archibald',
+        description: 'Mon meilleur amis depuis tout petit',
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg",
+        snapped: false,
+        location: 'Paris'
+      },
+      {
+        title: 'Testing',
+        description: '2eme component',
+        createdDate: new Date(),
+        snaps: 6,
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg",
+        snapped: false,
+        location: 'La montagne'
+      },
+      {
+        title: 'Bobby',
+        description: 'Enfin un ami',
+        createdDate: new Date(),
+        snaps: 23,
+        imageUrl: "https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg",
+        snapped: false
+      }
+    ];
   }
 }
